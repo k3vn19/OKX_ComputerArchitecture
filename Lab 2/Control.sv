9b6027755b62e51de997e87698dd6aa99649439c
okwong <35359342+okwong@users.noreply.github.com>
2019-05-15 16:57:58 -0700

    Update Controls

--- Lab 2/Control.sv
+++ Lab 2/Control.sv
@@ -1,10 +1,9 @@
 module Control(
   input [5:0] Instr_i,
-  output logic      RegDist,
-                    Jnz,
+  output logic      Jnz,
 							Jz,
 			        MemRead,
-			        MemtoReg,
+  output logic[1:0]  MemtoReg,
 			        MemWrite,
 			        ALUSrc,
 			        Regwrite,
@@ -12,7 +11,7 @@ module Control(
 );
 
 always_comb begin
-RegDist   = 0;
+//RegDist   = 0;
 Jnz      = 0;
 Jz			= 0;
 //Branch    = 0;
@@ -24,14 +23,39 @@ Regwrite  = 1;
 ALUOp     = 0;
 casez(Instr_i)
   6'b000000: begin	//load
-	MemtoReg = 1;
+	MemtoReg = 2'b10;
   end
   6'b000001: begin	 // store
 	MemWrite = 1;
+	RegWrite = 0;
+	MemRead = 0;
   end
-  6'b0000: begin
-   
+  6'b000010: begin	//XOR
+	ALUOp = 3'b011;
   end
+  6'b000011: begin	//JZ
+   Jz = 1;
+	RegWrite = 0;
+  end
+  6'b000100: begin		//Jnz
+   Jnz = 1;
+	RegWrite = 0;
+  end
+  6'b000101: begin		//get
+	MemtoReg = 2'b01;
+  end
+  6'b000110: begin		//right shft
+	ALUSrc = 1;
+	ALUOp = 3'b010;
+  end
+  6'b000111: begin		//left shft
+	ALUSrc = 1;
+	ALUOp = 3'b001;
+  end
+  6'b001000: begin	//ADD
+	ALUOp = 3'b000;
+  end
+
 endcase
 
 
