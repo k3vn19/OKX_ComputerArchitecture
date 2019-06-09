2cfac205f58a4473ce71d5755eee7aedd6b7004d
i54cxy <xic145@ucsd.edu>
2019-06-08 17:43:55 -0700

    updated indentation

--- Lab 2/pc.sv
+++ Lab 2/pc.sv
@@ -7,9 +7,8 @@ module pc (
 
   always_ff @(posedge clk) begin
     if (reset)
-	   pc_out <= 'b0;
-	 else
-		if (pc_in>1000) halt <= 1;
+      pc_out <= 'b0;
+    else if (pc_in>1000) halt <= 1;
     else
       pc_out <= pc_in;
   end
